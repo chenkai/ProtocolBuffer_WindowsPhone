ff977d2194f1ec7d6756c375f763c855690aaee0
chenkai <chenkaihome@live.cn>
2014-08-13 16:50:10 +0800

    empty project commit

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ProtocolBuffer_SampleDemo.Resources;
+
+namespace ProtocolBuffer_SampleDemo
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
